a5c0dbc17635984f890f674c55302e4b2c3665db
pedro-filadelfo <pedro03sp@gmail.com>
2025-03-16 14:54:27 -0300

    Teste OlaMundo

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
+function OlaMundo(numero) {
+    multi = numero*numero;
+    console.log (multi)
+    return multi
+    
+}
+
 
